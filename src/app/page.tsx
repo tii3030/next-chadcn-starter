f15336dc3b9e18490c8d3e5babe82e05faa3b12b
Tiago Pereira <tiiago.wy@gmail.com>
2025-05-13 15:07:58 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+export default async function Page() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    // return redirect('/auth/sign-in');
+    return redirect('/dashboard/overview');
+  } else {
+    redirect('/dashboard/overview');
+  }
+}
